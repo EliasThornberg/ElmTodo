3334ae9b77a564fa568e6b2b3c830e38dfe0cd27
EliasThornberg <elias_thornberg@hotmail.com>
2017-05-16 00:37:09 +0200

    Added button to remove TodoItem
    * Created functions for applying CSS to elements.
    * Need to get elm-format to work so we can format the code correctly....

--- Todo.elm
+++ Todo.elm
@@ -6,15 +6,11 @@ import Html.Keyed as Keyed
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
--- MODEL
 type alias Todo = { text: String, completed: Bool, id: Int }
 type alias Model = { items: List Todo, text: String, currentId: Int}
 
 model : Model
 model = { items= [], text = "", currentId = 0}
-
-
--- UPDATE
 
 type Msg 
   = AddTodo
@@ -32,29 +28,61 @@ update msg model =
       {model | items = List.filter (\m -> m.id /= id) model.items}
 
     CompleteTodo id ->
-      {model | items = List.map (setCompleted id) model.items }
+      {model | items = List.map (setTodoCompleted id) model.items }
     
     AddText s ->
       {model | text = s}
 
-setCompleted: Int -> Todo -> Todo
-setCompleted id todo =
+setTodoCompleted: Int -> Todo -> Todo
+setTodoCompleted id todo =
   if todo.id == id then {todo | completed = True} else todo
+
 
 view : Model -> Html Msg
 view model =
-  div [style [("display", "flex"), ("align-items", "left"), ("justify-content", "left")]][
+  div [style divStyle][
     input [ onInput AddText][]
-    ,button [ onClick AddTodo ][text "Lägg till"]
-    , buildTodoList (List.filter(\m -> m.completed == False) model.items)]
+    ,button [ onClick AddTodo ][text "Add todo"]
+    ,buildTodoList (List.filter showActiveTodo model.items)]
 
+showActiveTodo: Todo -> Bool
+showActiveTodo t =
+  t.completed == False
 
 buildTodoList: List Todo -> Html Msg
 buildTodoList list =
-    Keyed.ul [style [("list-style-type", "none"), ("align", "left")]] (List.map buildLi list)
+    Keyed.ul [style ulStyle] (List.map buildTodoListItem list)
 
-buildLi: Todo -> ( String, Html Msg )
-buildLi t =
-  (toString t.id , li [][input [type_ "Checkbox", onClick (CompleteTodo t.id), checked t.completed][], text t.text])
+buildTodoListItem: Todo -> ( String, Html Msg )
+buildTodoListItem t =
+  (toString t.id , li []
+    [
+      input [type_ "Checkbox", onClick (CompleteTodo t.id), checked t.completed][]
+      ,text t.text
+      ,button [onClick (DeleteTodo t.id), style removeButtonStyle][text "X"]
+    ])
 
 
+ulStyle: List(String, String)
+ulStyle =
+  [
+    ("list-style-type", "none")
+    ,("align", "left")
+  ]
+
+divStyle: List(String, String)
+divStyle =
+  [
+    ("display", "flex")
+    ,("align-items", "left")
+    ,("justify-content", "left")
+  ]
+
+removeButtonStyle: List(String, String)
+removeButtonStyle = 
+  [
+    ("width", "15px")
+    ,("text-align", "center")
+    ,("padding", "0px")
+    ,("margin-left", "15px")
+  ]
